0aac67669c945c5e9096b10e757cf75e3a735d68
nanotoylab <samuraidarkgamer@gmail.com>
2025-12-15 00:30:33 +0900

    削除機能の実装

---
+++ Blazor_Web_App/Blazor_Web_App/wwwroot/utility/confirmation.js
@@ -0,0 +1,7 @@
+﻿// wwwroot/utility/confirmation.js の中身
+
+// windowオブジェクトにこの関数を登録することで、C#から呼び出し可能になる
+window.confirmDelete = function (message) {
+    // 標準の confirm() ダイアログを表示し、結果の真偽値をそのまま返す
+    return confirm(message);
+};

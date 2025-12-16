4abb0ec2bc95e4ca0801b6e362a7c8b2d57c180d
nanotoylab <samuraidarkgamer@gmail.com>
2025-12-16 21:07:40 +0900

    メソッド名修正

--- Blazor_Web_App/Blazor_Web_App/wwwroot/utility/confirmation.js
+++ Blazor_Web_App/Blazor_Web_App/wwwroot/utility/confirmation.js
@@ -1,7 +1,7 @@
 ﻿// wwwroot/utility/confirmation.js の中身
 
 // windowオブジェクトにこの関数を登録することで、C#から呼び出し可能になる
-window.confirm = function (message) {
+window.yesNoConfirm = function (message) {
     // 標準の confirm() ダイアログを表示し、結果の真偽値をそのまま返す
     return confirm(message);
 };
